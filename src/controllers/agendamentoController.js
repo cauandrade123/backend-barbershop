cbd5f3f55e465e15ed22557b20df078639f3e139
Cauã Andrade Condé <caua.goldenboyy@gmail.com>
2026-01-21 22:50:56 -0300

    fix: agendamento

--- src/controllers/agendamentoController.js
+++ src/controllers/agendamentoController.js
@@ -1,12 +1,37 @@
 import { Router } from "express";
-import authenticatetoken from "../utils/jwt.js"
+import authenticateToken from "../utils/jwt.js"
+import MarcarServico from "../repository/agendamentoRepository.js"
 
 const endpoints = Router()
 
 
-endpoints.post(`/marcarservico`, authenticatetoken , (req,res)=>{
-    
-})
+endpoints.post('/marcarservico', authenticateToken, async (req, res) => {
+  try {
+
+    console.log("USER ID NO AGENDAMENTO:", req.userId);
+    console.log("BODY RECEBIDO:", req.body);
+
+    const escolhaServico = {
+      cliente_id: req.userId,
+      servico_id: req.body.servico_id,
+      data_agendamento: req.body.data_agendamento,
+      hora_agendamento: req.body.hora_agendamento
+    };
+
+    const SalvarServico = await MarcarServico(escolhaServico);
+
+    return res.status(201).json({
+      id: SalvarServico,
+      message: "Horário agendado!"
+    });
+
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ erro: "Erro interno" });
+  }
+});
+
+
 
 
 endpoints.get(`/listaragendamentos`, (req,res)=>{
